90cf238837aac18a61b628daa26906f1d6f45963
Koshti Sagar <107100610+sagar7778@users.noreply.github.com>
2024-05-11 11:50:12 +0530

    Files Upload

---
+++ src/components/about.tsx
@@ -0,0 +1,81 @@
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { SERVICES } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+
+type ServiceCardProps = {
+  index: number;
+  title: string;
+  icon: string;
+};
+
+// Service Card
+const ServiceCard = ({ index, title, icon }: ServiceCardProps) => {
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+      className="xs:w-[250px] w-full"
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+// About
+export const About = () => {
+  return (
+    <SectionWrapper idName="about">
+      <>
+        {/* Title */}
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Introduction</p>
+          <h2 className={styles.sectionHeadText}>Overview.</h2>
+        </motion.div>
+
+        {/* Body */}
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="empty-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          As a highly skilled and dedicated 3D Artist and Generalist with two
+          years of experience, I am known for my unwavering work ethic and
+          exceptional artistic abilities. Proficient in a wide range of
+          disciplines including 3D modeling, texturing, lighting, and rendering,
+          I am committed to delivering high-quality results in every project I
+          undertake. My excellent communication and teamwork skills enable me to
+          collaborate effectively with clients and colleagues, ensuring seamless
+          cooperation and outstanding outcomes. I am deeply committed to
+          continuous learning, consistently staying up-to-date with the latest
+          industry trends and advancements to maintain relevance and innovation
+          in my work. A hardworking professional driven by a passion for
+          creating visually stunning and engaging artwork, I approach each
+          project with enthusiasm and a commitment to excellence.
+        </motion.p>
+
+        {/* Service Card */}
+        <div className="mt-20 flex flex-wrap gap-10">
+          {SERVICES.map((service, i) => (
+            <ServiceCard key={service.title} index={i} {...service} />
+          ))}
+        </div>
+      </>
+    </SectionWrapper>
+  );
+};
